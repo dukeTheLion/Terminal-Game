20d3826eaddc5f3891a47cdf6c5c35511cc54c1f
Duke <savio_gm@icloud.com>
2021-08-25 18:17:56 -0300

    Drawer func

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 
 var clear map[string]func()
 
+var lines = 15
+var columns = 40
+
 func init() {
 	clear = make(map[string]func()) //Initialize it
 	clear["linux"] = func() {
@@ -33,9 +36,57 @@ func CallClear() {
 	}
 }
 
+func boxDrawer(text string, x int, y int) {
+	var outputString = ""
+
+	for i := 0; i < y; i++ {
+		for j := 0; j < x; j++ {
+			switch {
+			case i == 0 && j == 0:
+				outputString = "┏"
+				print(outputString)
+
+			case j > 0 && j < x-1 && (i == 0 || i == y-1):
+				outputString = "━"
+				print(outputString)
+
+			case i == 0 && j == x-1:
+				outputString = "┓"
+				print(outputString)
+
+			case i == y-1 && j == 0:
+				outputString = "┗"
+				print(outputString)
+
+			case i == y-1 && j == x-1:
+				outputString = "┛"
+				print(outputString)
+
+			default:
+				if j == 0 || j == x-1 {
+					outputString = "┃"
+				} else {
+					outputString = " "
+				}
+
+				print(outputString)
+			}
+		}
+		print("\n")
+	}
+
+}
+
+func drawer(text string) {
+	boxDrawer(text, columns, lines)
+	boxDrawer(text, columns, 5)
+}
+
 func loop() {
 	for {
 		CallClear()
+
+		drawer("")
 		time.Sleep(500 * time.Millisecond)
 		fmt.Print(".")
 		time.Sleep(500 * time.Millisecond)
